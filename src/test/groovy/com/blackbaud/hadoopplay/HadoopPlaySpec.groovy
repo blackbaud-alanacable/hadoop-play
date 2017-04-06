b5340672a9e2793f82443a1442db86bf88d26db4
Alana Cable <alana.cable@blackbaud.com>
2017-04-06 10:36:18 -0500

    add basic test

---
+++ src/test/groovy/com/blackbaud/hadoopplay/HadoopPlaySpec.groovy
@@ -0,0 +1,11 @@
+package com.blackbaud.hadoopplay
+
+import spock.lang.Specification
+
+class HadoopPlaySpec extends Specification {
+
+    def "should have a basic test" () {
+        expect:
+        true
+     }
+}
